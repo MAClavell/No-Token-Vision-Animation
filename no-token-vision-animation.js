c1afb661dae4fb606674fa29921fce6647c2afe8
MAClavell <michaelclavell11@gmail.com>
2021-01-13 16:57:34 -0500

    Refactored it all. Now uses libwrapper
    Don't like how this logic runs on every tick of the animation... Might have to do something about that

--- no-token-vision-animation.js
+++ no-token-vision-animation.js
@@ -1,100 +1,60 @@
-/* ------------------------------------------------------------------------ */
+'use strict';
 
-// Patching functions from "The Furnace" by KaKaRoTo
-// Patches.js
-// https://github.com/kakaroto/fvtt-module-furnace
-
-function patchClass(klass, func, line_number, line, new_line) {
-    // Check in case the class/function had been deprecated/removed
-    if (func === undefined)
-        return;
-    let funcStr = func.toString()
-    let lines = funcStr.split("\n")
-    if (lines[line_number].trim() == line.trim()) {
-        lines[line_number] = lines[line_number].replace(line, new_line);
-        let fixed = lines.join("\n")
-        if (klass !== undefined) {
-            let classStr = klass.toString()
-            fixed = classStr.replace(funcStr, fixed)
-        } else {
-            if (!(fixed.startsWith("function") || fixed.startsWith("async function")))
-                fixed = "function " + fixed
-            if (fixed.startsWith("function async"))
-                fixed = fixed.replace("function async", "async function");
-        }
-        return Function('"use strict";return (' + fixed + ')')();
-    } else {
-        console.log("Cannot patch function. It has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr)
-    }
-};
-
-function patchFunction(func, line_number, line, new_line) {
-    return patchClass(undefined, func, line_number, line, new_line)
-};
-
-function patchMethod(klass, func, line_number, line, new_line) {
-    return patchClass(klass, klass.prototype[func], line_number, line, new_line)
-};
-
-/* ------------------------------------------------------------------------ */
-
-function UpdateVisionCode() {
-    let newClass = Token;
-    newClass = patchMethod(newClass, "setPosition", 0,
-        `async setPosition(x, y, {animate=true}={}) {`,
-        `async setPosition(x, y, {animate=true, isGmUpdate=false}={}) {`);
-    if (!newClass) return;
-
-    newClass = patchMethod(newClass, "animateMovement", 0,
-        `async animateMovement(ray) {`,
-        `async animateMovement(ray, isGmUpdate=false) {`);
-    if (!newClass) return;
-
-    newClass = patchMethod(newClass, "setPosition", 23,
-        `if ( animate ) await this.animateMovement(new Ray(this.position, ray.B));`,
-        `if ( animate ) await this.animateMovement(new Ray(this.position, ray.B), isGmUpdate);`);
-    if (!newClass) return;
-
-    newClass = patchMethod(newClass, "animateMovement", 20,
-        `animate: game.settings.get("core", "visionAnimation"),`,
-        `animate: ( (game.settings.get("no-token-vision-animation", "disable_animation") == "none") || ((game.settings.get("no-token-vision-animation", "disable_animation") == "disableGM") && (game.user.isGM || !isGmUpdate)) ) && game.settings.get("core", "visionAnimation"),`);
-        //`animate: ( (game.settings.get("no-token-vision-animation", "disable_animation") == "none") || ((game.settings.get("no-token-vision-animation", "disable_animation") == "disableGM") && isGmUpdate && game.user.isGM) ) && game.settings.get("core", "visionAnimation"),`);
-    if (!newClass) return;
-
-    Token.prototype.setPosition = newClass.prototype.setPosition;
-    Token.prototype.animateMovement = newClass.prototype.animateMovement;
-}
+import {libWrapper} from './shim.js';
 
 // Initialize module
-Hooks.once('init', async function () {
-    console.log("Initializing \"No Token Vision Animation\"");
+Hooks.once('ready', function () {
+    const MODULE_NAME = "No Token Vision Animation";
+    const MODULE_ID = "no-token-vision-animation";
+    const SETTING_NAME = "disable_animation";
+    console.log(`Initializing "${MODULE_NAME}"`);
 
-    game.settings.register("no-token-vision-animation", "disable_animation", {
-        name: "Disable Token Vision Animation for all players",
-        hint: "Relative darkness in dim areas (0 to 1 where 1 is fully dark).",
+    game.settings.register(MODULE_ID, SETTING_NAME, {
+        name: game.i18n.localize("NTVA.SettingName"),
+        hint: game.i18n.localize("NTVA.SettingHint"),
         scope: "world",
         type: String,
         choices: {
-            "none": "None",
-            "disableAll": "Disable For All Players",
-            "disableGM": "Disable For GM",
+            "foundry": game.i18n.localize("NTVA.SettingFoundry"),
+            "disableAll": game.i18n.localize("NTVA.SettingDisableAll"),
+            "disableGM": game.i18n.localize("NTVA.SettingDisableGM"),
         },
-        default: "none",
+        default: "foundry",
         config: true,
     });
-});
 
-// Patch `Token.animateMovement` to restrict token vision animation
-Hooks.once('ready', function () {
-    UpdateVisionCode();
-
-    Hooks.on('preUpdateToken', (parent, entity, diff, options, user_id) => {
+    // Register to the 'preUpdateToken' hook
+    // Adds a 'isGmUpdate' property to the 'options' object, to be used later
+    // (the 'token' reference here is just a copy, not the actual Token)
+    Hooks.on('preUpdateToken', (parent, token, diff, options, user_id) => {
         options.isGmUpdate = game.users.entities.filter(u => u.isGM && u.id === user_id).length > 0;
-        //console.log(options);
     });
 
-    Hooks.on('updateToken', (parent, entity, diff, options, user_id) => {
-        console.log(options);
-    });
+    // Register the wrapper for Token._onUpdate
+    // This adds the 'isGmUpdate' field to the token so it can be used later
+    libWrapper.register(MODULE_ID, 'Token.prototype._onUpdate', function (wrapped, ...args) {
+        // args[1] is the 'options' parameter
+        this.isGmUpdate = args[1].isGmUpdate;
+
+        return wrapped(...args);
+    }, 'WRAPPER');
+
+    // Register the wrapper for Token._onMovementFrame
+    // This wrapper sets the config to actually disable the vision animation
+	libWrapper.register(MODULE_ID, 'Token.prototype._onMovementFrame', function (wrapped, ...args) {
+        let disableSetting = game.settings.get(MODULE_ID, SETTING_NAME);
+
+        // Disable if the vision animation if we need too
+        // args[2] is the 'config' parameter
+        if(disableSetting == "disableAll") {
+            args[2].animate = false;
+        }
+        else if(disableSetting == "disableGM") {
+            args[2].animate = args[2].animate && (game.user.isGM || !this.isGmUpdate);
+        }
+
+		// Call the original function
+		return wrapped(...args);
+	}, 'WRAPPER');
 });
 
